945ff7426760001862d8b062a2cf5baec283d60e
Adrian Urbanski <urbanski.adrian1703@gmail.com>
2023-05-24 23:48:11 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const allNavItems = document.querySelectorAll('.nav__link')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+navBtn.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
+
+allNavItems.forEach(item => item.addEventListener('click', () => navMobile.classList.remove('nav-mobile--active')))
+allNavItems.forEach(item => item.addEventListener('click', () => navBtn.classList.remove('is-active')))
